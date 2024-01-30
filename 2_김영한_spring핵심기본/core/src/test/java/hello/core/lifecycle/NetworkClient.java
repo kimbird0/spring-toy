592bb3a34695da60d065cf1443ff183b830a8c74
birdzero <redviking2@naver.com>
2024-01-30 22:09:20 +0900

    김영한 핵심기본 - @PostConstruct, @PreDestroy 를 통한 빈 생명주기

--- 2_김영한_spring핵심기본/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ 2_김영한_spring핵심기본/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,15 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+
+public class NetworkClient{
     private String url;
 
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url){
@@ -23,5 +26,20 @@ public class NetworkClient {
 
     public void disconnect(){
         System.out.println("close " + url);
+    }
+
+
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
