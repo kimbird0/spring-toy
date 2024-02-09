3790cf79256e645f26116992ee509cf40d7ec49d
birdzero <redviking2@naver.com>
2024-02-10 03:55:22 +0900

    김영한 MVC1 - http 요청 매핑

---
+++ 3_김영한_springMVC1/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,26 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+
+
+    @GetMapping(value = "/hello-basic")
+    public String mappingGetV2(){
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data){
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+}
