80ab791a5c33d0e2950792ba221836ffb5b2ee01
birdzero <redviking2@naver.com>
2024-02-14 22:23:03 +0900

    MVC2 - 타임리프 : text, utext (익스케이프, 언익스케이프)

---
+++ 5_springMVC2/thymeleaf-basic/src/main/java/hello/thymeleaf/ThymeleafBasicApplication.java
@@ -0,0 +1,15 @@
+package hello.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class ThymeleafBasicApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafBasicApplication.class, args);
+	}
+
+}
