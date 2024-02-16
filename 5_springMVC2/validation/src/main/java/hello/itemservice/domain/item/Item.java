afca9be0d2f904d91c9130f71e3c53b5f453b8ee
birdzero <redviking2@naver.com>
2024-02-17 00:16:30 +0900

    MVC2 - 밸리데이션 : 각 폼(밸리데이션) 마다 다른 객체 사용으로 해결

--- 5_springMVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ 5_springMVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,15 +11,15 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)// 수정 요구사항
+ //   @NotNull(groups = UpdateCheck.class)// 수정 요구사항
     private Long id;
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+  //  @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000)
+  //  @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+  //  @Range(min = 1000, max = 1000000)
     private Integer price;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class}) //수정 요구사항 추가
+  //  @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+  //  @Max(value = 9999, groups = {SaveCheck.class}) //수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
