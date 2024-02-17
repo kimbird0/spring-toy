0f043bdcb5851c45546711dabd517c84312d89c6
birdzero <redviking2@naver.com>
2024-02-18 05:52:17 +0900

    MVC2 - 예외처리 : 컨트롤러 코드에서 분리 @ControllerAdvice(like AOP)

--- 5_springMVC2/exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ 5_springMVC2/exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -14,25 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ApiExceptionV2Controller {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExHandler(IllegalArgumentException e){
-        log.error("[exceptionHandler] ex" , e);
-        return new ErrorResult("BAD", e.getMessage());
-    }
 
-    @ExceptionHandler
-    public ResponseEntity<ErrorResult> userExHandler(UserException e){
-        log.error("[exceptionHandler] ex");
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public ErrorResult exHandler(Exception e){
-        log.error("[exceptionHandler] ex");
-        return new ErrorResult("EX", "내부 오류");
-    }
 
     @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id){
