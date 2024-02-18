3662cac4d39433137265105f9bc48d880adda990
birdzero <redviking2@naver.com>
2024-02-19 00:03:51 +0900

    MVC2 - 타입 컨버터:  등록한 컨버터 어플리케이션에 적용(WebConfig)

--- 5_springMVC2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ 5_springMVC2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -5,6 +5,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistrar;
 import org.springframework.format.FormatterRegistry;
@@ -18,9 +19,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry){
 
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+       // registry.addConverter(new StringToIntegerConverter());
+       // registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new IpPortToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
