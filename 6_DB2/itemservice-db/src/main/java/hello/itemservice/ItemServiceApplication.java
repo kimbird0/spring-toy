246d01c3652bc899335e6d10494dfdd71f5625ee
birdzero <redviking2@naver.com>
2024-02-21 20:39:42 +0900

    DB2 - spring data JPA 적용2

--- 6_DB2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ 6_DB2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -14,7 +14,7 @@ import javax.sql.DataSource;
 
 
 @Slf4j
-@Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
