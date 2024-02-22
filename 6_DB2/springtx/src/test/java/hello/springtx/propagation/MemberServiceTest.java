f3e36ed0f3c9388c883b05ab294264f7e3dadb90
birdzero <redviking2@naver.com>
2024-02-22 22:39:06 +0900

    DB2 - 트랜잭션 : 비즈니스 로직에만 트랜잭션 적용 - 서비스 로직이 끝나야 롤백 or 커밋

--- 6_DB2/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ 6_DB2/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -58,4 +58,25 @@ class MemberServiceTest {
     }
 
 
+    /**
+     * memberService    @Transactional:ON
+     * memberRepository @Transactional:OFF
+     * logRepository    @Transactional:OFF
+     */
+    @Test
+    void singleTx(){
+        //given
+        String username = "outerTxOff_success";
+
+        //when
+
+        memberService.joinV1(username);
+
+        //when: 모든 데이터가 정상 저장
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+
+    }
+
+
 }
