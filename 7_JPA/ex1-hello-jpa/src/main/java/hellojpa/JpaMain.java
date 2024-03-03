66b80cef17123a9be6b04170881dcec01f680514
birdzero <redviking2@naver.com>
2024-03-03 16:26:32 +0900

    엔티티 매니저 예제

--- 7_JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 7_JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,29 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        //code
+        try {
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                            .getResultList();
+
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
