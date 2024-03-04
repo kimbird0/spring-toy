a1d8170315d3d54ef66bb81a8601c6eca80abe3b
birdzero <redviking2@naver.com>
2024-03-04 22:39:11 +0900

    JPA - 연관관계 매핑

--- 7_JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 7_JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import hellojpa.domain.Order;
 import jakarta.persistence.*;
 
 import java.util.List;
@@ -13,6 +14,10 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Order order = new Order();
+            order.addOrderItem(null);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- 7_JPA/ex1-hello-jpa/src/main/java/hellojpa/domain/Order.java
+++ 7_JPA/ex1-hello-jpa/src/main/java/hellojpa/domain/Order.java
@@ -1,51 +1,37 @@
 package hellojpa.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "ORDERS")
 public class Order {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ORDER_ID")
     private Long id;
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
     private LocalDateTime orderDate;
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public OrderStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(OrderStatus status) {
-        this.status = status;
+    public void addOrderItem(OrderItem o) {
+        orderItems.add((OrderItem) o);
+        o.setOrder(this);
     }
 }
